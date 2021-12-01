7b2315f9233dcd429227338b7b57db3821660d2b
Daniel Chen <hsueh.l.chen@outlook.com>
2021-12-01 11:58:54 -0800

    changes to select-data, added indexd to class

--- js/select-data.js
+++ js/select-data.js
@@ -59,107 +59,127 @@ let stepsData = [
 let faqData = [
   {
     // COVID-19
+    index: 1,
     heading: "What is COVID-19?",
     body: "COVID-19 is caused by the SARS-CoV-2 virus which is a new virus in humans causing a contagious respiratory illness. COVID-19 can present with a mild to severe illness, although some people infected with COVID-19 may have no symptoms at all. Older adults and people of any age who have underlying medical conditions have a higher risk of severe illness from COVID-19. Serious outcomes of COVID-19 include hospitalization and death. The SARS-CoV-2 virus can be spread to others not just while one is sick, but even before a person shows signs or symptoms of being sick (e.g. fever, coughing, difficulty breathing, etc.)."
   },
   {
+    index: 2,
     heading: "Where can we get the most updated information on COVID-19",
     body: "The most up-to-date information on COVID-19 is available at the CDC General webpage: https://www.cdc.gov/COVID19."
   },
   {
     // About GenBody COVID-19 Rapid Antigen Test
+    index:3,
     heading: "Does the test detect new SARS-CoV-2 Variants?",
     body: "Yes, The tests has two indirect pieces of evidence that indicate the GenBody COVID-19 Rapid Antigen Test performs well against the Delta variant. First, during laboratory testing, there were no changes in our calculated limit of detection. Second, we performed our anterior nasal trials in July, during which time the Delta variant was noted to be rampant. Our data showed improvment in sensitivity related to our nasopharyngeal trials which is a strong indictation that the Delta variant does not impact the performance of the tests. The Omicron variant was just recently discovered and no trials have yet to be conducted. "
   },
   {
+    index: 4,
     heading: "What are known potential risks for using the tests",
     body: "Possible discomfort or other complication taht can happen during sample collection and possible incorrect test result"
   },
   {
+    index: 5,
     heading: "What is serial testing?",
     body: "Serial testing is when one person is tested for COVID-19 more than once, as antigen test may be less sensitive than other COVID-19 tests and false results may occur. Repeated testing may identify result s more acurately than a single test. <br> After a single test, additional molecular COVID-19 test may be necessary depending on individual risk factors and test results."
   },
   {
+    index: 6,
     heading:
       "What is the difference between antigen tests and other COVID-19 tests?",
     body: "There are different kinds of test for diagnosing COVID-19. PCR tests or molecular tests detect genetic material from the virus. Antigen tests detect protein from the virus. Antigen tests are more specific for the virus being tested for but is less sensitive than the molecular test. This means that a positive result is highly accurate, but a negative result does not rule out infections. If the patient has recieved two negative tests within a 24 to 36 hours period, but is still experiencing COVID-19 symptoms such as coughing, shortness of breath, or fever, you should discuss the need for additional testing with the patient."
   },
   {
+    index: 7,
     heading: "Is this test FDA-approved or cleared?",
     body: "No. This test is not yet approved or cleared by the United States FDA. FDA may issue an Emergency Use Authorization (EUA) when certain criteria are met, which includes that there are no adequate, approved, available alternatives. The EUA for this test is supported by the Secretary of Health and Human Services (HHS’s) declaration that circumstances exist to justify the emergency use of in vitro diagnostics for the detection and/or diagnosis of the virus that causes COVID-19."
   },
   {
+    index: 8,
     heading: "What are the approved alternatives?",
     body: "a.	There are no approved available alternative antigen tests."
   },
   {
+    index: 9,
     heading: "How many times should the patient be tested to ensure accuracy?",
     body: "If the patient's first test is negative, you should test again in 24 to 46 hours to ensure the best accuracy."
   },
   {
+    index:10,
     heading: "Is the test reusable?",
     body: "No. Each test is intended for single use and is to be disposed of after the test is complete."
   },
   {
+    index:11,
     heading: "Where is the test made?",
     body: "The GenBody COVID-19 Rapid Ag Test is manufacatured in California, USA."
   },
   // Accuracy
   {
+    index: 12,
     heading: "How accurate is GenBody Antigen Rapid Test?",
     body: "GenBody Antigen Rapid Tests were compared to molecular tests (PCR) that were authorized by the FDA for SARS-CoV-2 assay to determine test performance. We have correctly identified 92% sensitivity and 99% specificity (false positive against false negative)"
   },
   {
+    index:13,
     heading: "What do I do if I have a positive result?",
     body: "If you have a positive test result, it is very likely that you have COVID-19 because protein from the virus were founding your sample. Therefore, it is also likely that you may be placed in isolation to avoid spreading the virus to others. There is a very small chance that this test can give a positive result that is wrong or false positive. Should you test positive with the GenBody Antigen Rapid Test, you should self-islate and seek follow-up care with your healthcare provider as additional testing may be necessary."
   },
   {
+    index: 14,
     heading: "What do I do next if I have a negative test result?",
     body: "A negative test result means that protein from the virus that causes the COVID-19 were not found in your sample. It is possible for this test to give a negative result that is incorrect. The amount of antigen in a sample may decrease the longer you have symptoms of infection. In symptomatic people, specimens collected after you have had symptoms for more than seven days may be more likely to be negative compared to a molecular assay. If you test negative and continue to experience COVID-19 like symptoms of fever, cough, and shortness of breath, you should seek follow up care with your healthcare provider and they may suggest you need another test to determine if you have contracted the virus causing COVID-19."
   },
   // Using GenBody Test
   {
+    index: 15,
     heading: "Where can I find the instructions for GenBody Antigen Rapid Tests?",
     body: "You can download our English Instructions here: https://www.fda.gov/media/150788/download"
   },
   {
+    index: 16,
     heading: "Will the test hurt?",
     body: "No, the gentle nasal swab is not sharp and it should not hurt. Only the lower part of the nostril should be swabbed. If the patient feels pain, please stop the test immediately."
   },
   {
+    index:17,
     heading: "What if the patient receives a positive result?",
     body: "A positive result means that it is very likely your patient has COVID-19 and it is important to instruct the patient to isolate themselves at home to avoid spreading the virus to others. There is a very small chance that this test can give positive results that are wrong. Please instruct the patient to follow CDC quarantine guidelines which can be found here: https://www.cdc.gov/coronavirus/2019-ncov/your-health/quarantine-isolation.html"
   },
   {
-    heading: "What do I do if my patient receives a negative test result?",
-    body: "A negative test result means that protein from the virus that causes the COVID-19 were not found in the patient's sample. It is possible for this test to give a negative result that is incorrect. The amount of antigen in a sample may decrease the longer your patient has symptoms of infection. In symptomatic people, specimen collected after your patient has had symptoms for more than seven days may be more likely to be negative compared to a molecular assay. If your patient tests negative and continues to experience COVID-19 like symptoms of fever, cough, and shortness of breath, please instruct your patient to seek follow-up care with their healthcare provider and they may suggest additional tests to determine if your patient has contracted the virus causing COVID-19. For more information about when to test, visit: https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/testing.html"
-  },
-  {
+    index: 18,
     heading: "What do I do if my patient receives a negative test result?",
     body: "If your patient received a second negative result in 24 to 36 hours after the first negative test, then your patient is likely not infected with COVID-19. If your patient has symptoms, they may have a different virus or type of infection. Your patient may have COVID-19  and still get a negative result, If the test was not performed correctly, such as not swabbing correctly or not waiting 15 minutes for test results. The level of antigen from the COVID-19 virus was below the test limits. If your patient have had signs and symptoms of COVID-19 for more than seven days. This means your patient can still have COVID-19 even though the test was negative. Please advice your patient to see their healthcare provider. Their healthcare provider will consider the test result along with all other aspect of their medical history, including symptoms specific to COVID-19 and decide how to care for them."
   },
   {
+    index: 19,
     heading: "What do I do if there is no control line when reading the result of my test?",
     body: "If you don't have a control line on the result of your test within 15-20 minutes, your test is invalid. Re-test with a new GenBody COVID-19 Ag and call the GenBody customer service at 888-552-5204 or email customer support at ts@genbodyamerica.com"
   },
   {
+    index:20,
     heading: "Is the extraction solution in the bottle harmful?",
     body: "No, the solution in the tube contains potentially harmful chemicals, however studies have shown them to be non-toxic at the elvel contained in the solution. The extraction solution should only be used as directed. Do not ingest, keep out of reach of children, and avoid contact wiht skin and eyes"
   },
   {
+    index:21,
     heading: "Who can be tested?",
-    body: "Individuals over 6 years of age with or without symptoms can be tested, as needed, regardless of vaccination status"
+    body: "There are no age restrictions on administration of our test, however, consent from parents or guardian for children too young to give consent."
   },
   {
+    index:22,
     heading: "Should people who were vaccinated use this test?",
     body: "Yes, even vaccinated individuals may carry the virus that could infect others. Testing along with vaccincations are important tools to prevent spreading of COVID-19. Frequent tesing is another way to reduce further spreading of the virus."
   },
   // Purchase
   {
+    index: 23,
     heading: "What is the requirement for purchasing GenBody COVID-19 Rapid Antigen Test?",
     body: "An authorized CLIA number and address with documentation is necessary before purchases can be shipped. Purchased tests can only be shipped to the designated address associated with the CLIA number."
   },
   {
+    index: 24,
     heading: "Who can we contact to purchase GenBody products",
     body: "For questions regarding GenBody Products, please contact us at our website or email us at ts@genbodyamerica.com"
   },
